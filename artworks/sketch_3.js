66a7a5a9db1ed1a6710d4621b287f5c10c005fb7
Jacek Grzegrzółka <jacekgrzegrzolka@gmail.com>
2023-11-26 20:26:16 +0100

    third artwork

--- artworks/sketch_3.js
+++ artworks/sketch_3.js
@@ -1,21 +1,21 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(500, 500);
     strokeWeight(0.5);
   }
   
-  function draw() {
-    background(220);
-    bs = 50;
-    
-    diff_i = 2
-    len = 50
-    
-    for(i=0;i<len;i++){
-        p1_x = 0
-        p2_x = width
-        p1_y = i*diff_i*i * 0.03
-        p2_y = height - p1_y
-        // p2_y = height
-        line(p1_x, p1_y, p2_x, p2_y)
-        
-      } 
+function draw() {
+background(220);
+bs = 50;
+
+diff_i = 2
+len = 50
+
+for(i=0;i<len;i++){
+    p1_x = 0
+    p2_x = width
+    p1_y = i*diff_i*i * 0.03
+    p2_y = height - p1_y
+    // p2_y = height
+    line(p1_x, p1_y, p2_x, p2_y)
+    }
+}
